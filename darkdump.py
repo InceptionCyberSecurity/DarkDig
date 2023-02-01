9ba41b4d9d959e2646e98021d691e0e0b97beb8c
ArcadeusOPS <info@arcadeusops.com>
2023-02-01 12:00:03 +0800

    Script edits

--- darkdump.py
+++ darkdump.py
@@ -23,11 +23,12 @@ SOFTWARE.
 This Code is based on DarkDump written by Josh Schiavone.
 '''
 # API needs optimising
+# output to JSON or html
 
 import sys
 sys.dont_write_bytecode = True
 
-__author__ = 'NAthan jones'
+__author__ = 'Nathan Jones'
 __version__ = '2.1'
 __license__ = 'MIT'
 
@@ -44,7 +45,7 @@ from banner.banner import Banner
 notice = '''
 Note:
     This tool is for Ethical Hacking Purposes. Stay Legal.
-    The author is not responsible for any misuse of DarkDig.
+    The author is not responsible for any misuse of DARKDIG.
     Stop The Rot!
 '''
 
@@ -62,22 +63,22 @@ class Colors:
     END = '\033[0m'
 
 class Configuration:
-    DARKDUMP_ERROR_CODE_STANDARD = -1
-    DARKDUMP_SUCCESS_CODE_STANDARD = 0
+    DARKDIG_ERROR_CODE_STANDARD = -1
+    DARKDIG_SUCCESS_CODE_STANDARD = 0
 
-    DARKDUMP_MIN_DATA_RETRIEVE_LENGTH = 1
-    DARKDUMP_RUNNING = False
-    DARKDUMP_OS_UNIX_LINUX = False
-    DARKDUMP_OS_WIN32_64 = False
-    DARKDUMP_OS_DARWIN = False
+    DARKDIG_MIN_DATA_RETRIEVE_LENGTH = 1
+    DARKDIG_RUNNING = False
+    DARKDIG_OS_UNIX_LINUX = False
+    DARKDIG_OS_WIN32_64 = False
+    DARKDIG_OS_DARWIN = False
 
-    DARKDUMP_REQUESTS_SUCCESS_CODE = 200
-    DARKDUMP_PROXY = False
+    DARKDIG_REQUESTS_SUCCESS_CODE = 200
+    DARKDIG_PROXY = False
 
     descriptions = []
     urls = []
 
-    __darkdump_api__ = "https://ahmia.fi/search/?q="
+    __DARKDIG_api__ = "https://ahmia.fi/search/?q="
     __proxy_api__ = "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=elite"
 
 class Platform(object):
@@ -90,13 +91,13 @@ class Platform(object):
 
         if self.execpltf:
             if sys.platform == "linux" or sys.platform == "linux2":
-                cfg.DARKDUMP_OS_UNIX_LINUX = True
+                cfg.DARKDIG_OS_UNIX_LINUX = True
                 print(clr.BOLD + clr.W + "Operating System: " + clr.G + sys.platform + clr.END)
             if sys.platform == "win64" or sys.platform == "win32":
-                cfg.DARKDUMP_OS_WIN32_64 = True
+                cfg.DARKDIG_OS_WIN32_64 = True
                 print(clr.BOLD + clr.W + "Operating System: " + clr.G + sys.platform + clr.END)
             if sys.platform == "darwin":
-                cfg.DARKDUMP_OS_DARWIN = True
+                cfg.DARKDIG_OS_DARWIN = True
                 print(clr.BOLD + clr.W + "Operating System: " + clr.G + sys.platform + clr.END)
         else: pass
 
@@ -114,7 +115,7 @@ class Proxies(object):
 
     def assign_proxy(self):
         req = requests.get(Configuration.__proxy_api__)
-        if req.status_code == Configuration.DARKDUMP_REQUESTS_SUCCESS_CODE:
+        if req.status_code == Configuration.DARKDIG_REQUESTS_SUCCESS_CODE:
             for line in req.text.splitlines():
                 if line:
                     proxy = line.split(':')
@@ -127,19 +128,19 @@ class Proxies(object):
     def get_proxy_dict(self):
         return self.proxy
 
-class Darkdump(object):
+class DARKDIG(object):
     def crawl(self, query, amount):
         clr = Colors()
         prox = Proxies()
 
         headers = random.choice(Headers().useragent)
-        if Configuration.DARKDUMP_PROXY == True:
+        if Configuration.DARKDIG_PROXY == True:
             prox.assign_proxy()
             proxy = prox.get_proxy()
             print(clr.BOLD + clr.P + "~:~ Using Proxy: " + clr.C + proxy + clr.END + '\n')
-            page = requests.get(Configuration.__darkdump_api__ + query, proxies=prox.get_proxy_dict())
+            page = requests.get(Configuration.__DARKDIG_api__ + query, proxies=prox.get_proxy_dict())
         else:
-            page = requests.get(Configuration.__darkdump_api__ + query)
+            page = requests.get(Configuration.__DARKDIG_api__ + query)
         page.headers = headers
 
         soup = BeautifulSoup(page.content, 'html.parser')
@@ -154,7 +155,7 @@ class Darkdump(object):
         Configuration.descriptions = list(dict.fromkeys(Configuration.descriptions))
         Configuration.urls = list(dict.fromkeys(Configuration.urls))
         try:
-            if len(Configuration.descriptions) >= Configuration.DARKDUMP_MIN_DATA_RETRIEVE_LENGTH:
+            if len(Configuration.descriptions) >= Configuration.DARKDIG_MIN_DATA_RETRIEVE_LENGTH:
                 for iterator in range(amount):
                     site_url = Configuration.urls[iterator]
                     site_description = Configuration.descriptions[iterator]
@@ -165,7 +166,7 @@ class Darkdump(object):
         except IndexError as ie:
             print(clr.BOLD + clr.O + f"[~] No more results to be shown ({ie}): " + clr.END)
 
-def darkdump_main():
+def DARKDIG_main():
     clr = Colors()
     cfg = Configuration()
     bn = Banner()
@@ -174,14 +175,14 @@ def darkdump_main():
     Platform(True).clean_screen()
     Platform(True).get_operating_system_descriptor()
     Proxies().assign_proxy()
-    bn.LoadDarkdumpBanner()
+    bn.LoadDARKDIGBanner()
     print(notice)
     time.sleep(1.3)
     # Set up argument parser
-    parser = argparse.ArgumentParser(description="Darkdump is a tool for searching the deep web for specific keywords. Made by yours truly.")
+    parser = argparse.ArgumentParser(description="DARKDIG is a tool for searching the deep web for specific keywords. Made by yours truly.")
     parser.add_argument("-v",
                         "--version",
-                        help="returns darkdump's version",
+                        help="returns DARKDIG's version",
                         action="store_true")
     parser.add_argument("-q",
                         "--query",
@@ -195,27 +196,27 @@ def darkdump_main():
 
     parser.add_argument("-p",
                         "--proxy",
-                        help="use darkdump proxy to increase anonymity",
+                        help="use DARKDIG proxy to increase anonymity",
                         action="store_true")
 
     args = parser.parse_args()
 
     if args.version:
-        print(clr.BOLD + clr.B + f"Darkdump Version: {__version__}\n" + clr.END)
+        print(clr.BOLD + clr.B + f"DARKDIG Version: {__version__}\n" + clr.END)
 
     if args.proxy:
-        Configuration.DARKDUMP_PROXY = True
+        Configuration.DARKDIG_PROXY = True
 
     if args.query and args.amount:
         print(clr.BOLD + clr.B + f"Searching For: {args.query} and showing {args.amount} results...\n" + clr.END)
-        Darkdump().crawl(args.query, args.amount)
+        DARKDIG().crawl(args.query, args.amount)
 
     elif args.query:
         print(clr.BOLD + clr.B + f"Searching For: {args.query} and showing 10 results...\n" + clr.END)
-        Darkdump().crawl(args.query, 10)
+        DARKDIG().crawl(args.query, 10)
 
     else:
         print(clr.BOLD + clr.O + "[~] Note: No query arguments were passed. Please supply a query to search. " + clr.END)
 
 if __name__ == "__main__":
-    darkdump_main()
+    DARKDIG_main()
