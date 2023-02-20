2421686ec59df4f3e5efacf552ea623e9dd102b1
ArcadeusOPS <info@arcadeusops.com>
2023-02-20 21:08:22 +0800

    Update darkdump.py

--- darkdump.py
+++ darkdump.py
@@ -1,34 +1,12 @@
-'''
-MIT License
-Copyright (c) 2023 ArcadeusOPS
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-This Code is based on DarkDump written by Josh Schiavone.
-'''
+# MIT License
+# This Code is based on DarkDump written by Josh Schiavone.
 # API needs optimising
 # output to JSON or html or TXT; format
 
 import sys
 sys.dont_write_bytecode = True
 
-__author__ = 'Nathan Jones'
+__author__ = 'Nathan Jones based on Jash Schiavone'
 __version__ = '2.1'
 __license__ = 'MIT'
 
