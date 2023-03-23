e51ca37c5727f93d7f0389db6d42e308f9d5f3bc
Arcadeus OPS <info@arcadeusops.com>
2023-03-23 13:21:17 +0800

    Update darkdump.py

--- darkdump.py
+++ darkdump.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 # MIT License
-# This Code is based on DarkDump written by Josh Schiavone.
-# API needs optimising
-# output to JSON or html or TXT; format
-
+# This Code is based on DarkDump written by Josh Schiavone; API needs optimising; output to JSON or html or TXT
+# Additions by Nathan Jones nathan.jones@arcadeusops.com
 import sys
 sys.dont_write_bytecode = True
 
-__author__ = 'Nathan Jones based on Jash Schiavone'
+__author__ = 'Nathan Jones based on Josh Schiavone'
 __version__ = '2.1'
 __license__ = 'MIT'
 
@@ -17,9 +15,14 @@ import os
 import time
 import argparse
 import random
+import email, smtplib, ssl
 
 from headers.agents import Headers
 from banner.banner import Banner
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 notice = '''
 Note:
@@ -199,4 +202,53 @@ def DARKDIG_main():
 
 if __name__ == "__main__":
     DARKDIG_main()
-# save as XML then convert to HTML; email to user defined address
+
+
+# save as XML then convert to HTML
+document.pdf 
+
+
+# email to user defined address
+subject = "Dark Web Scraping Results"
+body = "Results of the DarkDig Python App"
+sender_email = "info@arcadeusops.com"
+receiver_email = "nathan.jones@arcadeusops.com"
+password = input("Type your password and press enter:")
+
+# Create a multipart message and set headers
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message["Bcc"] = receiver_email  # Recommended for mass emails
+
+# Add body to email
+message.attach(MIMEText(body, "plain"))
+
+filename = "document.pdf"  # In same directory as script
+
+# Open PDF file in binary mode
+with open(filename, "rb") as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+# Encode file in ASCII characters to send by email
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
+
+# Add attachment to message and convert message to string
+message.attach(part)
+text = message.as_string()
+
+# Log in to server using secure context and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, text)
